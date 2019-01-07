35db3c21f50c688d3a7e23b8c95fddc70d742e88
Timur Kashapov <timshako@gmail.com>
2019-01-07 19:30:31 +0300

    добавлен пользовательский интерфейс + checkstyle maven plugin

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,7 @@
 import com.shako.app.Download;
+import com.shako.app.DownloadManager;
 import com.shako.app.State;
+import javax.swing.*;
 
 /**
  * Запускатель.
@@ -10,22 +12,24 @@ public class Runner {
     {}
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(DownloadManager::new);
 
-        String title = "NewDownload";
+
+        //String title = "NewDownload";
 
         // ---------------------------------------------------
         // добавляем новый процесс загрузки:
 
         // java legacy
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                new Download();
-            }
-        }, title).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                new Download();
+//            }
+//        }, title).start();
 
         // lava
-        new Thread(() -> new Download(State.INIT), title).start();
+        // new Thread(() -> new Download(State.INIT), title).start();
 
         // lava+mref
         // new Thread(Download::new, title).start();
--- src/main/java/com/shako/app/DownloadManager.java
+++ src/main/java/com/shako/app/DownloadManager.java
@@ -1,4 +1,36 @@
 package com.shako.app;
 
-public class DownloadManager {
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.Toolkit;
+
+/**
+ * Пользовательский интерфейс и управление процессом загрузки.
+ */
+public class DownloadManager extends JFrame  {
+
+    private DownloadsTableModel model;
+    private ProgressRenderer renderer;
+
+    public DownloadManager() {
+        setTitle("Javka Download Manager");
+        setEnabled(Boolean.TRUE);
+        setDefaultLookAndFeelDecorated(Boolean.TRUE);
+        setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(Boolean.TRUE);
+    }
+
+    public DownloadsTableModel getModel() {
+        return model;
+    }
+    public void setModel(DownloadsTableModel model) {
+        this.model = model;
+    }
+    public ProgressRenderer getRenderer() {
+        return renderer;
+    }
+    public void setRenderer(ProgressRenderer renderer) {
+        this.renderer = renderer;
+    }
 }
